fcbaef06283d22be590cd71460ffc248edf41591
Yash Sangwan <77121057+Yashsangwan2002@users.noreply.github.com>
2024-03-26 15:05:07 +0530

    Time: 0 ms (100%), Space: 10.7 MB (79.77%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        sort(citations.begin(),citations.end());
+        int ans=0;
+        for(int i=0;i<n;i++){
+             if(citations[i] >= n-i) {
+             ans = n-i; 
+            break; 
+             };
+        };
+        return ans;
+    }
+};
